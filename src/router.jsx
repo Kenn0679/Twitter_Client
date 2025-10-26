4549e625ae370a8bf564cecd51e2a455db99d970
Kenn0679 <kenn0679@gmail.com>
2025-10-26 13:44:07 +0700

    configureer --global core.editor "code --wait"

--- src/router.jsx
+++ src/router.jsx
@@ -1,16 +1,16 @@
-import { createBrowserRouter } from "react-router-dom";
-import Home from "./Home";
-import Login from "./Login";
+import { createBrowserRouter } from 'react-router-dom';
+import Home from './Home';
+import Login from './Login';
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Home />,
-	},
-	{
-		path: "/login/oauth",
-		element: <Login />,
-	},
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/login/oauth',
+    element: <Login />
+  }
 ]);
 
 export default router;
