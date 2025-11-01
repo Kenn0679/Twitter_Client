7363a6d0cef926b13fbd2b0a6ebaff6e5733f0f9
Kenn0679 <kenn0679@gmail.com>
2025-11-01 07:30:43 +0700

    refactor: improve code formatting and consistency in Home component

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,66 +1,66 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { Link } from "react-router-dom";
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { Link } from 'react-router-dom';
 
 const getGoogleAuthURL = () => {
-	const { VITE_GOOGLE_CLIENT_ID, VITE_GOOGLE_REDIRECT_URI } = import.meta.env;
-	const url = `https://accounts.google.com/o/oauth2/v2/auth`;
-	const query = {
-		client_id: VITE_GOOGLE_CLIENT_ID,
-		redirect_uri: VITE_GOOGLE_REDIRECT_URI,
-		response_type: "code",
-		scope: [
-			"https://www.googleapis.com/auth/userinfo.profile",
-			"https://www.googleapis.com/auth/userinfo.email",
-		].join(" "),
-		prompt: "consent",
-		access_type: "offline",
-	};
+  const { VITE_GOOGLE_CLIENT_ID, VITE_GOOGLE_REDIRECT_URI } = import.meta.env;
+  const url = `https://accounts.google.com/o/oauth2/v2/auth`;
+  const query = {
+    client_id: VITE_GOOGLE_CLIENT_ID,
+    redirect_uri: VITE_GOOGLE_REDIRECT_URI,
+    response_type: 'code',
+    scope: ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'].join(
+      ' '
+    ),
+    prompt: 'consent',
+    access_type: 'offline'
+  };
 
-	const queryString = new URLSearchParams(query).toString();
-	return `${url}?${queryString}`;
+  const queryString = new URLSearchParams(query).toString();
+  return `${url}?${queryString}`;
 };
 
 const googleOAuthURL = getGoogleAuthURL();
 
 export default function Home() {
-	const isAuthenticated = Boolean(localStorage.getItem("access_token"));
-	const logout = () => {
-		localStorage.removeItem("access_token");
-		localStorage.removeItem("refresh_token");
-		window.location.reload();
-	};
+  const isAuthenticated = Boolean(localStorage.getItem('access_token'));
+  const logout = () => {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
+    window.location.reload();
+  };
 
-	return (
-		<>
-			<div>
-				<span>
-					{" "}
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</span>
+  return (
+    <>
+      <div>
+        <span>
+          {' '}
+          <img src={viteLogo} className='logo' alt='Vite logo' />
+        </span>
 
-				<span>
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</span>
-			</div>
-			<h1>Google Oauth 2.0</h1>
+        <span>
+          <img src={reactLogo} className='logo react' alt='React logo' />
+        </span>
+      </div>
+      <video controls width={'85%'}>
+        <source src='http://localhost:5000/static/videos-stream/ckzvks8491n1tc6ae27vqyfdq.mp4' type='video/mp4' />
+      </video>
+      <h1>Google Oauth 2.0</h1>
 
-			<p className="read-the-docs">
-				{isAuthenticated ? (
-					<div>
-						<span style={{ display: "block" }}>
-							You are logged in successfully!
-						</span>
-						<button onClick={logout}>Logout</button>
-					</div>
-				) : (
-					<div>
-						<span style={{ display: "block" }}>Please log in to continue.</span>
-						<Link to={googleOAuthURL}>Login with Google</Link>
-					</div>
-				)}
-			</p>
-		</>
-	);
+      <div className='read-the-docs'>
+        {isAuthenticated ? (
+          <div>
+            <span style={{ display: 'block' }}>You are logged in successfully!</span>
+            <button onClick={logout}>Logout</button>
+          </div>
+        ) : (
+          <div>
+            <span style={{ display: 'block' }}>Please log in to continue.</span>
+            <Link to={googleOAuthURL}>Login with Google</Link>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
